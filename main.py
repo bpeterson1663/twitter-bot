d898d029b50afac1bccfb70e0074fad13dc8341e
Brady Peterson <bpeterson1663@gmail.com>
2021-10-09 21:05:12 -0500

    feat(twitter): get and loop through tweets

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print('twitter api bot')
- ## need to aply for developer license for twitter api
+import tweepy
+
+auth = tweepy.OAuthHandler("API_KEY", "API_TOKEN")
+auth.set_access_token('ACCESS_TOKEN', 'access_token_secret')
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
