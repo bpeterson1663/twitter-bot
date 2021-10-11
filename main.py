5433930124ebef1cdb0b9e9b5b9a0f00699fcb05
Brady Peterson <bpeterson1663@gmail.com>
2021-10-10 21:43:09 -0500

    feat(twitter): access user information

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ auth = tweepy.OAuthHandler("API_KEY", "API_TOKEN")
 auth.set_access_token('ACCESS_TOKEN', 'access_token_secret')
 
 api = tweepy.API(auth)
+user = api.me()
 
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
+
+print(user.name)
+print(user.followers_count)
